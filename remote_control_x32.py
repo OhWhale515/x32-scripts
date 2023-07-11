e5dcb49ae867bb44da218c563249aae9dce4dd6f
OhWhale515 <sterlinggreen515@gmail.com>
2023-07-11 15:00:58 -0400

    added bus n mtx linx , link configs,mono mode/link

--- remote_control_x32.py
+++ remote_control_x32.py
@@ -41,28 +41,93 @@ mixer.send("/ch/01/mix/on", 0)
 print(mixer.query("/ch/01/mix/on"))
 
 # Channel Link CH1-CH2 On
-mixer.send("/config/chlink/1-2", 1)
-print(mixer.query("/config/chlink/1-2", 1))
+mixer.send("/config/chlink/1-2/on", 1)
+print(mixer.query("/config/chlink/1-2/on", 1))
 
 # Channel Link CH1-CH2 Off
-mixer.send("/config/chlink/1-2", 0)
-print(mixer.query("/config/chlink/1-2", 0))
+mixer.send("/config/chlink/1-2/on", 0)
+print(mixer.query("/config/chlink/1-2/on", 0))
 
 # AUX Link AUX1-AUX2 On
-mixer.send("/config/auxlink/1-2", 1)
-print(mixer.query("/config/auxlink/1-2", 1))
+mixer.send("/config/auxlink/1-2/on", 1)
+print(mixer.query("/config/auxlink/1-2/on", 1))
 
 # AUX Link AUX1-AUX2 Off
-mixer.send("/config/auxlink/1-2", 0)
-print(mixer.query("/config/auxlink/1-2", 0))
+mixer.send("/config/auxlink/1-2/on", 0)
+print(mixer.query("/config/auxlink/1-2/on", 0))
 
 # FX Link FX1-FX2 On
-mixer.send("/config/fxlink/1-2", 1)
-print(mixer.query("/config/fxlink/1-2", 1))
+mixer.send("/config/fxlink/1-2/on", 1)
+print(mixer.query("/config/fxlink/1-2/on", 1))
 
 # FX Link FX1-FX2 Off
-mixer.send("/config/fxlink/1-2", 0)
-print(mixer.query("/config/fxlink/1-2", 0))
+mixer.send("/config/fxlink/1-2/on", 0)
+print(mixer.query("/config/fxlink/1-2/on", 0))
+
+# Group Selection Mute On
+mixer.send("/config/mute/1/on", 1)
+print(mixer.query("/config/mute/1/on", 1))
+
+# Group Selection Mute Off
+mixer.send("/config/mute/1/on", 0)
+print(mixer.query("/config/mute/1/on", 0))
+
+# mtx link  On
+mixer.send("/config/mtxlink/1/on", 1)
+print(mixer.query("/config/mtxlink/1/on", 1))
+
+# mtx link  Off
+mixer.send("/config/mtxlink/1/on", 0)
+print(mixer.query("/config/mtxlink/1/on", 0))
+
+# link config hadly On/ Sets Delay & HA link
+mixer.send("/config/linkcfg/hadly/on", 1)
+print(mixer.query("/config/mtxlink/1/on", 1))
+
+# link config hadly Off
+mixer.send("/config/linkcfg/hadly/on", 0)
+print(mixer.query("/config/mtxlink/1/on", 0))
+
+# link config EQ On/ Sets EQ link
+mixer.send("/config/linkcfg/eq/on", 1)
+print(mixer.query("/config/linkcfg/1/on", 1))
+
+# link config EQ Off/ Removes EQ link
+mixer.send("/config/linkcfg/eq/on", 0)
+print(mixer.query("/config/linkcfg/eq/on", 0))
+
+# link config dynamics On
+mixer.send("/config/linkcfg/dyn/on", 1)
+print(mixer.query("/config/linkcfg/dyn/on", 1))
+
+# link config dynamics Off
+mixer.send("/config/linkcfg/dyn/on", 0)
+print(mixer.query("/config/linkcfg/dyn/on", 0))
+
+# link config FDR Mute On
+mixer.send("/config/linkcfg/fdrmute/on", 1)
+print(mixer.query("/config/linkcfg/fdrmute/on", 1))
+
+# link config FDR Mute Off
+mixer.send("/config/linkcfg/fdrmute/on", 0)
+print(mixer.query("/config/linkcfg/fdrmute/on", 0))
+
+
+# Mono Mode LR+M 
+mixer.send("/config/mono/mode", 1)
+print(mixer.query("/config/mono/mode", 1))
+
+# Mono Mode LCR 
+mixer.send("/config/mono/mode", 0)
+print(mixer.query("/config/mono/mode", 0))
+
+# Mono Mode Link On
+mixer.send("/config/mono/link", 1)
+print(mixer.query("/config/mono/link", 1))
+
+# Mono Mode Link Off
+mixer.send("/config/mono/link", 0)
+print(mixer.query("/config/mono/link", 0))
 
 
 
