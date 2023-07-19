06ea0178c5472cc79431151c0ab153c16a140768
OhWhale515 <sterlinggreen515@gmail.com>
2023-07-18 20:21:24 -0400

    revised script

--- x32.py
+++ x32.py
@@ -1,86 +1,72 @@
-import random
 import time
+import xair_api
+import random
 
+kind_id = "X32"
+ip = "192.168.1.247"
+
+mixer = xair_api.connect(kind_id, ip=ip)
+mixer.__enter__()
+
+print(mixer.validate_connection())
 def toggle_gate_on_channel(mixer, channel):
-    # Toggle GATE on specified channel (1 or more)
     mixer.send(f"/ch/{channel:02}/gate/on", 1)
-    print(mixer.query(f"/ch/{channel:02}/gate/on"))
     time.sleep(1)
     on = True
     return on
 
 def set_gate_threshold(mixer, channel, threshold):
-    # Set GATE threshold on specified channel (1 or more)
     mixer.send(f"/ch/{channel:02}/gate/thr", threshold)
-    print(mixer.query(f"/ch/{channel:02}/gate/on"))
     time.sleep(1)
 
 def toggle_gate_off_channel(mixer, channel):
-    # Toggle GATE off on specified channel (1 or more)
     mixer.send(f"/ch/{channel:02}/gate/on", 0)
-    print(mixer.query(f"/ch/{channel:02}/gate/on"))
     time.sleep(1)
 
 def toggle_dynamics_on_channel(mixer, channel):
-    # Toggle DYNAMICS on specified channel (1 or more)
     mixer.send(f"/ch/{channel:02}/dyn/on", 1)
-    print(mixer.query(f"/ch/{channel:02}/dyn/on"))
     time.sleep(1)
     on = True
     return on
 
 def toggle_dynamics_off_channel(mixer, channel):
-    # Toggle DYNAMICS off on specified channel (1 or more)
     mixer.send(f"/ch/{channel:02}/dyn/on", 0)
-    print(mixer.query(f"/ch/{channel:02}/dyn/on"))
     time.sleep(1)
 
-# Example usage:
-channel = 1
-mixer = YourMixerObject()  # Replace YourMixerObject with the actual mixer object you have
-
-# Toggle GATE on channel 1
-on_state = toggle_gate_on_channel(mixer, channel)
-
-# Generate random GATE threshold values between -60 and 10
-gate_threshold_values = [random.uniform(-60, 10) for _ in range(100)]
-
-# Set GATE Threshold on channel 1 using the random values
-for threshold in gate_threshold_values:
-    set_gate_threshold(mixer, channel, threshold)
-    on_state = ~on_state  # Toggle on/off state for the next iteration
-
-# Toggle GATE off channel 1
-toggle_gate_off_channel(mixer, channel)
-
-# Toggle DYNAMICS on channel 1
-on_state = toggle_dynamics_on_channel(mixer, channel)
-
-# Generate random DYNAMICS threshold values between -60 and 10
-dynamics_threshold_values = [random.uniform(-60, 10) for _ in range(100)]
-
-# Set DYNAMICS Threshold on channel 1 using the random values
-for threshold in dynamics_threshold_values:
-    mixer.send(f"/ch/{channel:02}/dyn/thr", str(threshold))
-    on_state = ~on_state  # Toggle on/off state for the next iteration
-
-# Toggle DYNAMICS off channel 1
-toggle_dynamics_off_channel(mixer, channel)
-
-def toggle_eq_on_channel(mixer, channel):
-    # Toggle EQ on specified channel (1 or more)
-    mixer.send(f"/ch/{channel:02}/eq/on", 1)
-    print(mixer.query(f"/ch/{channel:02}/eq/on"))
+def channel_link_on(mixer, channel1, channel2):
+    mixer.send(f"/config/chlink/{channel1}-{channel2}/on", 1)
     time.sleep(1)
 
-def set_eq_band_type(mixer, channel, band, eq_type):
-    # Set EQ type for a specific band on the specified channel (1 or more)
-    mixer.send(f"/ch/{channel:02}/eq/{band}/{eq_type}", 1)
-
-def toggle_eq_off_channel(mixer, channel):
-    # Toggle EQ off on specified channel (1 or more)
-    mixer.send(f"/ch/{channel:02}/eq/on", 0)
-    print(mixer.query(f"/ch/{channel:02}/eq/on"))
+def channel_link_off(mixer, channel1, channel2):
+    mixer.send(f"/config/chlink/{channel1}-{channel2}/on", 0)
     time.sleep(1)
-    
-    
+
+def aux_link_on(mixer, aux1, aux2):
+    mixer.send(f"/config/auxlink/{aux1}-{aux2}/on", 1)
+    time.sleep(1)
+
+def aux_link_off(mixer, aux1, aux2):
+    mixer.send(f"/config/auxlink/{aux1}-{aux2}/on", 0)
+    time.sleep(1)
+
+def fx_link_on(mixer, fx1, fx2):
+    mixer.send(f"/config/fxlink/{fx1}-{fx2}/on", 1)
+    time.sleep(1)
+
+def fx_link_off(mixer, fx1, fx2):
+    mixer.send(f"/config/fxlink/{fx1}-{fx2}/on", 0)
+    time.sleep(1)
+
+def group_selection_mute_on(mixer, group):
+    mixer.send(f"/config/mute/{group}/on", 1)
+    time.sleep(1)
+
+def group_selection_mute_off(mixer, group):
+    mixer.send(f"/config/mute/{group}/on", 0)
+    time.sleep(1)
+
+def generate_random_threshold_values(count, lower_bound=-60, upper_bound=10):
+    return [random.uniform(lower_bound, upper_bound) for _ in range(count)]
+
+
+
